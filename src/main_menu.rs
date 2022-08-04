b5bba1ff987942f9571d5a91203b430a4ec88f31
Matias Klemola <matias.klemola@gmail.com>
2022-08-04 14:31:43 +0300

    Add keyboard/gamepad navigation to main menu

--- src/main_menu.rs
+++ src/main_menu.rs
@@ -5,8 +5,8 @@ use crate::app::{cursor_visible, AppState, ButtonPress, VetovoimaColor, APP_NAME
 const BUTTON_COLOR: Color = VetovoimaColor::BLUEISH_DARK;
 const BUTTON_COLOR_HOVER: Color = VetovoimaColor::BLUEISH_MID;
 const BUTTON_ACTIVE_COLOR: Color = VetovoimaColor::BLUEISH_LIGHT;
-static NEW_GAME_BUTTON_LABEL: &str = "New game (joystick button)";
-static EXIT_BUTTON_LABEL: &str = "Exit (left front button)";
+static NEW_GAME_BUTTON_LABEL: &str = "New game";
+static EXIT_BUTTON_LABEL: &str = "Exit";
 
 #[derive(Component)]
 pub enum MenuEvent {
@@ -14,7 +14,7 @@ pub enum MenuEvent {
     BeginNewGame,
 }
 
-#[derive(Component)]
+#[derive(Component, PartialEq, Clone, Copy, Debug)]
 enum MenuButton {
     NewGame,
     Exit,
@@ -23,11 +23,15 @@ enum MenuButton {
 #[derive(Component)]
 struct MainMenu;
 
+#[derive(Component)]
+struct SelectedButton(Option<MenuButton>);
+
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<MenuEvent>()
+            .insert_resource(SelectedButton(None))
             .add_system_set(
                 SystemSet::on_enter(AppState::InMenu)
                     .with_system(show_menu)
@@ -35,8 +39,9 @@ impl Plugin for MainMenuPlugin {
             )
             .add_system_set(
                 SystemSet::on_update(AppState::InMenu)
-                    .with_system(menu_button_state)
-                    .with_system(menu_input_event),
+                    .with_system(mouse_interaction)
+                    .with_system(selected_button_change)
+                    .with_system(button_press),
             )
             .add_system_set(SystemSet::on_exit(AppState::InMenu).with_system(hide_menu));
     }
@@ -45,11 +50,13 @@ impl Plugin for MainMenuPlugin {
 fn show_menu(
     mut commands: Commands,
     mut menu_event: EventWriter<MenuEvent>,
+    mut selected_button: ResMut<SelectedButton>,
     asset_server: Res<AssetServer>,
 ) {
     let font = asset_server.load("VT323-Regular.ttf");
     let font_size = 64.0;
 
+    selected_button.0 = None;
     menu_event.send(MenuEvent::EnterMenu);
 
     commands
@@ -97,7 +104,7 @@ fn show_menu(
             menu_node
                 .spawn_bundle(ButtonBundle {
                     style: Style {
-                        size: Size::new(Val::Px(720.0), Val::Px(80.0)),
+                        size: Size::new(Val::Px(400.0), Val::Px(80.0)),
                         justify_content: JustifyContent::Center,
                         align_items: AlignItems::Center,
                         margin: Rect::all(Val::Px(10.0)),
@@ -125,7 +132,7 @@ fn show_menu(
             menu_node
                 .spawn_bundle(ButtonBundle {
                     style: Style {
-                        size: Size::new(Val::Px(720.0), Val::Px(80.0)),
+                        size: Size::new(Val::Px(400.0), Val::Px(80.0)),
                         justify_content: JustifyContent::Center,
                         align_items: AlignItems::Center,
                         margin: Rect::all(Val::Px(10.0)),
@@ -157,56 +164,101 @@ fn hide_menu(mut commands: Commands, menu: Query<Entity, With<MainMenu>>) {
     commands.entity(menu).despawn_recursive();
 }
 
-fn menu_button_state(
+fn mouse_interaction(
     mut interaction_query: Query<
-        (&Interaction, &mut UiColor),
+        (&Interaction, &MenuButton, &mut UiColor),
         (Changed<Interaction>, With<MenuButton>),
     >,
+    selected_button: Res<SelectedButton>,
+    mut app_state: ResMut<State<AppState>>,
+    mut menu_event: EventWriter<MenuEvent>,
+    mut exit: EventWriter<AppExit>,
 ) {
-    for (interaction, mut color) in interaction_query.iter_mut() {
+    for (interaction, button, mut color) in interaction_query.iter_mut() {
         match *interaction {
             Interaction::Clicked => {
                 *color = BUTTON_ACTIVE_COLOR.into();
+
+                match button {
+                    MenuButton::NewGame => {
+                        menu_event.send(MenuEvent::BeginNewGame);
+
+                        app_state
+                            .set(AppState::InitGame)
+                            .expect("Could not start the game")
+                    }
+                    MenuButton::Exit => exit.send(AppExit),
+                };
             }
             Interaction::Hovered => {
                 *color = BUTTON_COLOR_HOVER.into();
             }
             Interaction::None => {
-                *color = BUTTON_COLOR.into();
+                *color = match selected_button.0 {
+                    Some(selected) if selected == *button => BUTTON_ACTIVE_COLOR.into(),
+
+                    _ => BUTTON_COLOR.into(),
+                };
             }
         }
     }
 }
 
-fn menu_input_event(
-    interaction_query: Query<(&Interaction, &MenuButton), (Changed<Interaction>, With<Button>)>,
+fn selected_button_change(
+    mut menu_buttons_query: Query<(&MenuButton, &mut UiColor)>,
+    selected_button: Res<SelectedButton>,
+) {
+    if selected_button.is_changed() {
+        for (button, mut color) in menu_buttons_query.iter_mut() {
+            *color = match selected_button.0 {
+                Some(selected) if selected == *button => BUTTON_ACTIVE_COLOR.into(),
+
+                _ => BUTTON_COLOR.into(),
+            };
+        }
+    }
+}
+
+fn button_press(
     button_press: Res<ButtonPress>,
+    mut selected_button: ResMut<SelectedButton>,
     mut app_state: ResMut<State<AppState>>,
     mut menu_event: EventWriter<MenuEvent>,
     mut exit: EventWriter<AppExit>,
 ) {
-    for (interaction, button) in interaction_query.iter() {
-        if *interaction == Interaction::Clicked {
-            match button {
-                MenuButton::NewGame => {
-                    menu_event.send(MenuEvent::BeginNewGame);
-
-                    app_state
-                        .set(AppState::InitGame)
-                        .expect("Could not start the game")
-                }
-                MenuButton::Exit => exit.send(AppExit),
-            }
-        }
+    if !button_press.is_changed() {
+        return;
     }
 
-    if button_press.select_pressed {
-        exit.send(AppExit)
-    } else if button_press.main_control_pressed {
-        menu_event.send(MenuEvent::BeginNewGame);
+    if button_press.main_control_pressed {
+        match selected_button.0 {
+            Some(MenuButton::NewGame) => {
+                menu_event.send(MenuEvent::BeginNewGame);
 
-        app_state
-            .set(AppState::InitGame)
-            .expect("Could not start the game")
+                app_state
+                    .set(AppState::InitGame)
+                    .expect("Could not start the game")
+            }
+
+            Some(MenuButton::Exit) => exit.send(AppExit),
+
+            _ => (),
+        }
+    } else if button_press.up_pressed || button_press.down_pressed {
+        selected_button.0 = Some(select_next_button(selected_button.0));
+    }
+}
+
+fn select_next_button(selected_button: Option<MenuButton>) -> MenuButton {
+    match selected_button {
+        Some(button) => {
+            if button == MenuButton::NewGame {
+                MenuButton::Exit
+            } else {
+                MenuButton::NewGame
+            }
+        }
+
+        None => MenuButton::NewGame,
     }
 }
