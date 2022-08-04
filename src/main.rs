5fa4844584762a334809d5a3bc2cfbfce7c6c519
Matias Klemola <matias.klemola@gmail.com>
2022-08-04 14:36:50 +0300

    Restore window and render properties

--- src/main.rs
+++ src/main.rs
@@ -30,8 +30,8 @@ fn main() {
         .insert_resource(WindowDescriptor {
             title: APP_NAME.into(),
             width: 1280.0,
-            height: 1024.0,
-            mode: WindowMode::SizedFullscreen,
+            height: 720.0,
+            mode: WindowMode::Windowed,
             resizable: false,
             ..default()
         })
